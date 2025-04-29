55a00395d78edfb48de52516c7ffab2e8c655b82
solmee <kii5541@naver.com>
2025-04-30 02:40:58 +0900

    feat: 버스 번호 입력 페이지 퍼블리싱

--- src/app/BusSearch/page.tsx
+++ src/app/BusSearch/page.tsx
@@ -1,1 +1,17 @@
-export default function BusSearch() {}
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const BusSearchFunnel = dynamic(() => import('./BusSearchFunnel'), {
+  ssr: false,
+});
+
+export default function BusSearchPage() {
+  return (
+    <div className="flex min-h-dvh w-full items-center justify-center">
+      <div className="w-full max-w-md">
+        <BusSearchFunnel />
+      </div>
+    </div>
+  );
+}
